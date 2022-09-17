0772b8ca8bca2be9013ee3cee699931c5cd699ad
qw372 <55595159+qw372@users.noreply.github.com>
2022-09-17 15:17:24 -0500

    update

--- Srf-Rb-ODT/python code/test.py
+++ Srf-Rb-ODT/python code/test.py
@@ -1,1 +1,7 @@
-print("{:0<8.3g}".format(2.5))
+import numpy as np
+
+c = 299792458 # SI units
+hbar = 1.05457182e-34 # SI units
+omega = 2*np.pi*c/780e-9 # SI units
+omega_0 = 2*np.pi*c/1064e-9 # SI units
+
